41f8164d32d62cf636092ed1802e4ee6f426d553
Sumedh  Dubey <sumedhdubey872@gmail.com>
2025-08-14 17:45:41 +0530

    AR business card

---
+++ Assets/OpenURL.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void youtube()
+    {
+        Application.OpenURL("https://www.youtube.com/channel/UCJ2Nl7vKA9-j6795l6egubA?view_as=subscriber");
+    }
+    public void twitter()
+    {
+        Application.OpenURL("https://twitter.com/GameHou53267722");
+    }
+    public void linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/game-house-35a1261b0/");
+    }
+}
